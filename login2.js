1acc148c64d668761085321944092f1f65bc8a9a
Ernest "Hayden" Gaines <110562217+HaydenAndAki@users.noreply.github.com>
2022-10-02 01:53:27 -0400

    Update login2.js

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ function validate()
 {
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
-if(username=="admin"&& password=="user")
+if(username=="Hayden"&& password=="aki")
 {
     alert("Login Successfully");
     <script> if (window.opener){window.opener.location.href="https://haydenandaki.github.io/Website-Development/"; window.close();}</script>
