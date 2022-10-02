e316e3db8d566c1d72e5733d5b4358809a8f40e6
Ernest "Hayden" Gaines <110562217+HaydenAndAki@users.noreply.github.com>
2022-10-02 01:27:29 -0400

    Create login2.js

---
+++ login2.js
@@ -0,0 +1,17 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin"&& password=="user")
+{
+    alert("Login Successfully");
+    window.open(https://haydenandaki.github.io/Website-Development/)
+    return false;
+}
+else
+{
+    alert("Login Failed");
+}
+
+
+}
